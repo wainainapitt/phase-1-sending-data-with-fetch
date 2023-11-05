8e309ab5a4557e4b990d1827c9ad51971b217bc6
peter <pngugi.wainaina@gmail.com>
2023-11-05 18:29:17 +0300

    lab done

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-function submitData( name, email ) {
-
-    return fetch( 'http://localhost:3000/users', {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          "Accept": "application/json"
-        },
-        body: JSON.stringify( {
+function submitData (name , email){
+  return fetch("http://localhost:3000/users", {
+  method: "POST",
+  headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+  },
+      body: JSON.stringify({
           name,
           email
-        } )
-      } )
-      .then( function ( response ) {
-        return response.json()
-      } )
-      .then( function ( object ) {
-        document.body.innerHTML = object[ "id" ]
-      } )
-      .catch( function ( error ) {
-        document.body.innerHTML = error.message
-      } )
-  }
+      }),
+  })
+  .then( (response)=>response.json())
+  .then( (res)=>res.json())
+  .then(function (funny) {
+      return document.body.innerHTML = funny["id"]
+      })
+      .catch(function (error) {
+      alert(error.message);
+      console.log(error.message);
+      document.body.innerHTML = error.message;
+    })
+}
